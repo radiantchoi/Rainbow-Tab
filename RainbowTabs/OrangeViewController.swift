7bd1271daeec255f16243cafb556c1df844777bb
Gordon Choi <thefirate@gmail.com>
2021-04-26 03:58:28 +0900

    Modify view controllers for every tab

---
+++ RainbowTabs/OrangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrangeViewController.swift
+//  RainbowTabs
+//
+//  Created by Gordon Choi on 2021/04/26.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
